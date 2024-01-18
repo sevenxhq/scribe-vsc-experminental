3d4978cdd8d00871b21e74172b3c1571d0d3724a
Kalisa <95277748+Kalisa11@users.noreply.github.com>
2024-01-18 10:19:39 +0200

    add licence & language (#3)

---
+++ src/shared/projectTypes.ts
@@ -0,0 +1,20 @@
+export type Language = {
+    lc: string;
+    ld: string;
+    alt: string[];
+    hc: string;
+    ln: string;
+    ang: string;
+    lr: string;
+    pk: number;
+    gw: boolean;
+    cc: string[];
+  };
+  
+  export type Copyright = {
+    title: string;
+    id: string;
+    licence: string;
+    locked: boolean;
+  };
+  
--- webview-ui/src/views/Sidebar.tsx
+++ webview-ui/src/views/Sidebar.tsx
@@ -4,15 +4,23 @@ import {
   VSCodeButton,
   VSCodeTextArea,
   VSCodeTextField,
+  VSCodeDropdown,
+  VSCodeOption,
 } from "@vscode/webview-ui-toolkit/react";
+import { Copyright, Language } from "../../../src/shared/projectTypes";
 import { MessageType } from "../../../src/shared/messageTypes";
 import { vscode } from "../utilities/vscode";
+import staticLangs from "../../../src/resources/langNames.json";
+import advancedSettings from "../../../src/resources/AdvanceSettings.json";
 
 const Sidebar = () => {
   const [projectName, setProjectName] = useState("");
   const [description, setDescription] = useState("");
   const [abbreviation, setAbbreviation] = useState("");
-  // const [language, setLanguage] = useState();
+  const languagesList = staticLangs;
+  const licenseList = advancedSettings.copyright;
+  const [copyright, setCopyright] = useState<Copyright>();
+  const [language, setLanguage] = useState<Language>();
 
   const handleSubmit = () => {
     vscode.postMessage({
@@ -21,6 +29,8 @@ const Sidebar = () => {
         projectName,
         description,
         abbreviation,
+        language,
+        copyright,
       },
     });
   };
@@ -65,7 +75,7 @@ const Sidebar = () => {
             onChange={(e) => {
               setDescription((e.target as HTMLTextAreaElement).value);
             }}
-            className="w-48 lg:w-full h-28  block rounded shadow-sm sm:text-sm focus:border-primary"
+            className="w-48 lg:w-full h-28 block rounded shadow-sm sm:text-sm focus:border-primary"
           />
           {/* <span className="text-error">{error.description[0]?.message}</span> */}
         </div>
@@ -92,24 +102,36 @@ const Sidebar = () => {
               </span> */}
             </div>
           </div>
-          {/* <div className="flex gap-5 mt-5 items-center">
+          <div className="flex gap-5 mt-5 items-center">
             <div>
               <div className="flex gap-4 items-center mb-2">
                 <h4 className="text-xs font-base  text-primary  tracking-wide leading-4  font-light">
-                  {t("label-target-language")}
-                  <span className="text-error">*</span>
+                  Target Language <span className="text-error">*</span>
                 </h4>
               </div>
-              <CustomMultiComboBox
-                selectedList={[language]}
-                setSelectedList={setLanguage}
-                customData={languages}
-                filterParams="ang"
-                dropArrow
-                showLangCode={{ show: true, langkey: "lc" }}
-              />
+
+              <VSCodeDropdown
+                position="below"
+                className="w-48 lg:w-full"
+                value={language ? language?.ang : "Select Language"}
+                onChange={(e) => {
+                  setLanguage(
+                    languagesList.find(
+                      (lang) =>
+                        lang.ang === (e.target as HTMLInputElement).value
+                    )
+                  );
+                }}
+              >
+                {languagesList.slice(0, 100).map((lang) => (
+                  <VSCodeOption value={lang.ang} key={lang.pk}>
+                    {lang.ang} {""}
+                    <span className="text-green-600">({lang.lc})</span>
+                  </VSCodeOption>
+                ))}
+              </VSCodeDropdown>
             </div>
-            <button
+            {/* <button
               type="button"
               className="mt-6 -ml-2"
               title={t("msg-min-three-letter")}
@@ -118,8 +140,8 @@ const Sidebar = () => {
             </button>
             <div className="mt-5">
               <TargetLanguagePopover projectType={headerDropDown} />
-            </div>
-          </div> */}
+            </div> */}
+          </div>
           {/* <div className="mt-5">
             <button
               type="button"
@@ -136,6 +158,35 @@ const Sidebar = () => {
               replaceConformation={callReplace}
             />
           </div> */}
+          <div className="flex gap-5 mt-5 items-center">
+            <div>
+              <div className="flex gap-4 items-center mb-2">
+                <h4 className="text-xs font-base text-primary tracking-wide leading-4 font-light">
+                  Licence <span className="text-error">*</span>
+                </h4>
+              </div>
+
+              <VSCodeDropdown
+                position="below"
+                className="w-48 lg:w-full"
+                value={copyright ? copyright.title : "Select License"}
+                onChange={(e) => {
+                  setCopyright(
+                    licenseList.find(
+                      (license) =>
+                        license.title === (e.target as HTMLInputElement).value
+                    )
+                  );
+                }}
+              >
+                {licenseList.map((licence) => (
+                  <VSCodeOption value={licence.title} key={licence.id}>
+                    {licence.title}
+                  </VSCodeOption>
+                ))}
+              </VSCodeDropdown>
+            </div>
+          </div>
         </div>
 
         <div>
